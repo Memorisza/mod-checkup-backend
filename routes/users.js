a181d17e3db6fda79b4c9ee146df582683619e71
Memorisza <aut14030@hotmail.com>
2021-11-25 13:57:03 +0700

    [UPDATE] Users
    -Added more security
    -Rename symbols

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import checkAuthorize from '../_helpers/checkAuthorize.js'
+import Role from '../_helpers/role.js'
 
 import { createUser, findUserById, getAllUsers, updateUser, getCurrentUser } from '../controllers/users.js'
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
+router.get('/', checkAuthorize(Role.Admin), getAllUsers);
 router.get('/current', checkAuthorize(), getCurrentUser);
-router.get('/:userId', findUserById);
+router.get('/:userId', checkAuthorize(Role.Admin), findUserById);
 // router.put('/:id', updateUser);
 
 export default router;
